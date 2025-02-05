f53a97c9caae5ca3ffc0ad1409f19385871c599b
André Vinícius <andrev.macambira@gmail.com>
2025-02-05 11:43:19 -0300

    Primeiro commit - projeto criado

---
+++ src/main/java/com/andre/cursomongo/CursMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.andre.cursomongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursMongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andre/cursomongo/CursMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andre.cursomongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
