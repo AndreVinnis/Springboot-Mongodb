4a8eb6370ac1899be1e44eb2d8cd69776e7c124c
André Vinícius <andrev.macambira@gmail.com>
2025-02-12 10:58:53 -0300

    Endpoint para retornar os posts dw um usuario

--- src/main/java/com/andre/cursomongo/resources/UserResource.java
+++ src/main/java/com/andre/cursomongo/resources/UserResource.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.andre.cursomongo.domain.Post;
 import com.andre.cursomongo.domain.User;
 import com.andre.cursomongo.dto.UserDTO;
 import com.andre.cursomongo.services.UserService;
@@ -60,5 +61,11 @@ public class UserResource {
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build(); 
+	}
+	
+	@GetMapping(value = "/{id}/posts")
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id){
+		User obj = service.findById(id); 
+		return ResponseEntity.ok().body(obj.getPosts()); 
 	}
 }
