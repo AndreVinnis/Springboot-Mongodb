74bc8b81883f9d4bce58ec23439e3d6c99f4441b
André Vinícius <andrev.macambira@gmail.com>
2025-02-12 11:09:55 -0300

    Obtendo post por id

---
+++ src/main/java/com/andre/cursomongo/services/PostService.java
@@ -0,0 +1,21 @@
+package com.andre.cursomongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.andre.cursomongo.domain.Post;
+import com.andre.cursomongo.repository.PostRepository;
+import com.andre.cursomongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	@Autowired
+	private PostRepository repo; 
+
+	public Post findById(String id) { 
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
